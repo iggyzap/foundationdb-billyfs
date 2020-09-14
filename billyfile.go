7965ad42bde66b970305f6cce0042b82160a3556
Ignat Zapolsky <izapolsky@gmail.com>
2020-09-14 21:01:49 +0100

    Now it's compilable. Need some unit tests

--- billyfile.go
+++ billyfile.go
@@ -1,10 +1,13 @@
 package billyfs
 
 import (
+	"os"
+
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	"github.com/go-git/go-billy/v5"
 )
 
+// FoundationDbFile represents a file in foundation db
 type FoundationDbFile struct {
 	fs              *FoundationDbFs
 	path            string
@@ -13,9 +16,16 @@ type FoundationDbFile struct {
 
 var _ billy.File = FoundationDbFile{}
 
+// NewFile creates a struct
 func NewFile(fs FoundationDbFs, path string) (FoundationDbFile, error) {
 	// allocates new logical file
 	return FoundationDbFile{&fs, path, 0}, nil
+}
+
+// Open does nothing
+//does nothing. Need to initialise attrs
+func (file FoundationDbFile) Open(flag int, perm os.FileMode) (billy.File, error) {
+	return file, nil
 }
 
 func (FoundationDbFile) Read(p []byte) (n int, err error) {
@@ -23,33 +33,64 @@ func (FoundationDbFile) Read(p []byte) (n int, err error) {
 	return 0, nil
 }
 
-const READ_SIZE int8 = 4096
+const rEADSIZE int16 = 4096
 
-
-func (file FoundationDbFile) ReadAt(p []byte, off int64) (n int, err error) {
-
-
-	return file.fs.db.ReadTransact( func(tx fdb.ReadTransaction) {
-		tx.GetRange()
-
-	
-	)
+func findDataKey(path string, offset int64) string {
+	return ""
 }
 
+// Write writes bytes in write postion. Stateful!
+func (FoundationDbFile) Write(p []byte) (int, error) {
+
+	return 0, nil
+}
+
+// ReadAt function that is directly compatible with stateless NFS
+func (file FoundationDbFile) ReadAt(p []byte, off int64) (d int, e error) {
+
+	tmp, error := file.fs.db.ReadTransact(func(tx fdb.ReadTransaction) (interface{}, error) {
+		return tx.Get(fdb.Key(findDataKey(file.path, off))).MustGet(), nil
+	})
+
+	if error != nil {
+		return 0, error
+	}
+
+	if tmp != nil {
+		return copy(tmp.([]byte), p), nil
+	}
+
+	return 0, nil
+}
+
+// Seek is not compatible with NFSv3 Only makes sense in context of writing because Write is stateful
+func (FoundationDbFile) Seek(offset int64, i int) (int64, error) {
+	return 0, nil
+}
+
+// Truncate truncates file
+func (FoundationDbFile) Truncate(size int64) error {
+	return nil
+}
+
+// Close have no meaning in NFSv3
 func (FoundationDbFile) Close() error {
 	//does nothing
 
 	return nil
 }
 
+// Lock is not supported
 func (FoundationDbFile) Lock() error {
 	return nil
 }
 
+// Unlock is not supported
 func (FoundationDbFile) Unlock() error {
 	return nil
 }
 
+// Name returns file name
 func (file FoundationDbFile) Name() string {
 	return file.path
 }
--- billyfs.go
+++ billyfs.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-git/go-billy/v5"
 )
 
+// FoundationDbFs representds a billy filesystem over FoundationDb KV store
 type FoundationDbFs struct {
 	db fdb.Database
 }
@@ -16,7 +17,7 @@ var _ billy.Basic = FoundationDbFs{}
 var _ billy.Dir = FoundationDbFs{}
 var _ billy.Capable = FoundationDbFs{}
 
-// Creates new FoundationDBFs
+// NewFoundationDbFs Creates new FoundationDBFs
 func NewFoundationDbFs(clusterFile string) (FoundationDbFs, error) {
 	//fdb.setAPIVersion
 	db, error := fdb.OpenDatabase(clusterFile)
@@ -29,45 +30,64 @@ func NewFoundationDbFs(clusterFile string) (FoundationDbFs, error) {
 }
 
 //billy.Dir methods
+
+// MkdirAll creates full path
 func (FoundationDbFs) MkdirAll(path string, perm os.FileMode) error {
 	return nil
 }
 
+// ReadDir returns all file entries in a pth
 func (FoundationDbFs) ReadDir(path string) ([]os.FileInfo, error) {
 	return nil, nil
 }
 
 //billy.Basic methods
+
+// Open  a file
 func (fs FoundationDbFs) Open(path string) (billy.File, error) {
 	return fs.OpenFile(path, os.O_RDONLY, 0666)
 }
 
+// Create creates a file
 func (fs FoundationDbFs) Create(path string) (billy.File, error) {
 
 	return fs.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0666)
 }
 
+// OpenFile full fledged call
 func (fs FoundationDbFs) OpenFile(path string, flag int, perm os.FileMode) (billy.File, error) {
-	return NewFile(fs, path).Open(flag, perm)
+
+	file, error := NewFile(fs, path)
+	if error == nil {
+		return file.Open(flag, perm)
+	}
+
+	return file, error
 }
 
+// Remove deletes path
 func (FoundationDbFs) Remove(path string) error {
 	return nil
 }
 
+// Rename renames path
 func (FoundationDbFs) Rename(from string, to string) error {
 	return nil
 }
 
+// Stat obtains file meta
 func (FoundationDbFs) Stat(path string) (os.FileInfo, error) {
 	return nil, nil
 }
 
+// Join joins path
 func (FoundationDbFs) Join(arr ...string) string {
 	return ""
 }
 
 //billy.Capable methods
+
+// Capabilities what fs can do
 func (FoundationDbFs) Capabilities() billy.Capability {
 	return billy.ReadAndWriteCapability | billy.SeekCapability | billy.TruncateCapability
 
