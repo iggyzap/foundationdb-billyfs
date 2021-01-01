5b7e49b7da7be49075c16cef69fcaf233ea411ba
Ignat Zapolsky <ignat.zapolsky@hertz.com>
2021-01-01 22:29:12 +0000

    Started writing test for FS.

---
+++ billyfs_test.go
@@ -0,0 +1,86 @@
+package billyfs
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	docker "github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	pkg_errors "github.com/pkg/errors"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestOpenFs(t *testing.T) {
+	defer handleError(t)
+
+	var dir = t.TempDir()
+	var filePath, err = startFdb(dir, t)
+	checkError(err, "Failed starting fdb for %s", filePath)
+
+	//var _, error = NewFoundationDbFs(filePath)
+	//checkError(error, "Failed creating Fs %s for %s", error, filePath)
+
+}
+
+//this function catches panic and signals to testing framework that test have failed
+func handleError(t *testing.T) {
+	if r := recover(); r != nil {
+		if err, ok := r.(error); ok {
+			t.Error(err)
+
+		} else {
+			panic(r)
+		}
+	}
+}
+
+func checkError(err error, s string, args ...interface{}) {
+	if err != nil {
+		panic(pkg_errors.Wrapf(err, s, args...))
+	}
+}
+
+// Starts foundation db container for specific test run. Defers container removal when test is finished.
+// WORK in progress
+func startFdb(s string, t *testing.T) (string, error) {
+	var cli, err = docker.NewClientWithOpts()
+	checkError(err, "Failed to create docker client")
+
+	resp, err := cli.ImagePull(context.TODO(), "foundationdb/foundationdb:6.2.25", types.ImagePullOptions{})
+	defer resp.Close()
+	checkError(err, "Failed to pull image")
+
+	conf := container.Config{Image: "foundationdb/foundationdb:6.2.25", ExposedPorts: nat.PortSet{"4500/tcp": {}}}
+
+	cnt, err := cli.ContainerCreate(context.TODO(),
+		&conf,
+		&container.HostConfig{},
+		&network.NetworkingConfig{},
+		nil,
+		t.Name())
+
+	checkError(err, "Failed to create docker container")
+	t.Logf("Created container with config %+v\n", conf)
+	t.Cleanup(func() {
+		err := cli.ContainerRemove(context.TODO(), cnt.ID, types.ContainerRemoveOptions{RemoveVolumes: true, RemoveLinks: false, Force: true})
+		checkError(err, "Unable to kill container %s", cnt.ID)
+	})
+
+	err = cli.ContainerStart(context.TODO(), cnt.ID, types.ContainerStartOptions{})
+	checkError(err, "Failed to start container %s", cnt.ID)
+
+	//run DB init with file. optionally supply different entry point.
+
+	return "nil", nil
+}
+
+func killFdb() {
+
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,17 @@ go 1.15
 
 require (
 	github.com/apple/foundationdb/bindings/go v0.0.0-20200910163956-0b8a4fcd16c7
+	github.com/containerd/containerd v1.4.3 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v20.10.1+incompatible
+	github.com/docker/go-connections v0.4.0
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-git/go-billy/v5 v5.0.0
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	google.golang.org/grpc v1.34.0 // indirect
 )
